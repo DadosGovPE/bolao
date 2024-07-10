b8d4b2bd7cfe1bedb837069a6fa6e7d1434face8
Gilberto <g.trindade@gmail.com>
2024-07-10 00:56:32 -0300

    alterando o serializer do user para podemos criar um ususario com senha

--- bolao/serializers.py
+++ bolao/serializers.py
@@ -3,9 +3,19 @@ from rest_framework import serializers
 from .models import Team, Game, Bet
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'password']
+        
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data.get('email')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
